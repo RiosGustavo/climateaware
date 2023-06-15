057a86ca249081fb399e1057b0e89ba0465a7809
aguslorca <aguslorca.12@gmail.com>
2023-06-15 20:19:58 -0300

    Climate Aware v 0.1

---
+++ src/main/java/com/egg/climateAware/ClimateAwareApplication.java
@@ -0,0 +1,13 @@
+package com.egg.climateAware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateAwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateAwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egg/climateAware/ClimateAwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.climateAware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateAwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
