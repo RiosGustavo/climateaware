66b17fc1235dad2d10e716921ed165564508fef7
RiosGustavo <gustavosuperlango@gmail.com>
2023-06-20 18:19:37 -0500

    eliminamos usuario

--- src/main/java/com/egg/climateAware/entidades/Empresa.java
+++ src/main/java/com/egg/climateAware/entidades/Empresa.java
@@ -1,15 +1,14 @@
-
 package com.egg.climateAware.entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
 
 @Entity
 @Getter
@@ -18,13 +17,13 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @ToString
 
-public class Empresa extends Usuario  {
-    
-   private String nombreEmpresa;
+public class Empresa extends Usuario {
+
+    private String nombreEmpresa;
     private String cuit;
     private String direccion;
     private String rubro;
-    
-    @ManyToOne
+
+    @OneToMany
     private Campaña campañas;
 }
