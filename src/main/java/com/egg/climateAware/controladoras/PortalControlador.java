c90d0f445c63a82b1be9d61177a35ed2f4cda6ff
aguslorca <aguslorca.12@gmail.com>
2023-06-21 18:30:30 -0300

    Index, login, register

--- src/main/java/com/egg/climateAware/controladoras/PortalControlador.java
+++ src/main/java/com/egg/climateAware/controladoras/PortalControlador.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/")
@@ -36,12 +39,31 @@ public class PortalControlador {
     }
     
     
-      @GetMapping("/login")
-    public String login(String error, ModelMap modelo) {
-        if (error!=null){
-            modelo.put("error", "Mail o contraseña incorrectos");
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, ModelMap modelo) {
+        
+        if (error != null) {
+            modelo.put("error", "Usuario o contraseña inválidos");
         }
-        return "login_form.html";
+        return "login.html";
     }
 
+     @GetMapping("/registrar")
+    public String registrar() {
+
+        return "register.html";
+    }
+    @PostMapping("/registro")
+    public String registro(MultipartFile archivo,@RequestParam String email, @RequestParam String password1, @RequestParam String password2, ModelMap modelo) {
+        try {
+            usuarioServicio.crearUsuario(archivo,email, password1, password2);
+            modelo.put("exito", "¡Felicitaciones! Ya sos parte de Climate Aware");
+            return "register.html";
+        } catch (Exception e) {
+            modelo.put("error", e.getMessage());
+            modelo.put("email", email);
+            return "register.html";
+        }
+
+    }
 }
